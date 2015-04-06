3016db910a222580c13219d31c371c180695c565
Alex Rock <hi@lxrck.com>
2015-04-06 11:35:05 -0500

    removed some stuff in the auth

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,6 @@
 // config/auth.js
 
 	module.exports = {
-
-		'amazon': {
- 			'key': process.env.S3_KEY,
-			'secret': process.env.S3_SECRET,
-			'bucket': process.env.S3_BUCKET
-		},
 		
 		'instagram': {
 			'client_id': process.env.INSTAGRAM_CLIENT_ID,
